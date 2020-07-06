42cb08aadb78e460bdd5150e54582d240d228b31
MohanrajValluvan <MohanrajValluvan@9.77.202.65>
2020-07-06 11:44:41 +0530

    Mobile Testing Test Case Completion status

--- src/browser/amazon_1.java
+++ src/browser/amazon_1.java
@@ -26,11 +26,18 @@ public class amazon_1 {
 				AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
 				driver.manage().timeouts().implicitlyWait(30,  TimeUnit.SECONDS);
 				driver.get("http://amazon.in");
-				driver.findElement(By.xpath("//*[@id=\"nav-search-keywords\"]")).sendKeys("iphone 7s plus");
-				driver.findElement(By.xpath("//*[@id=\"nav-search-form\"]/div[2]/div/input")).click();
-				driver.findElement(By.xpath("//*[@id=\"search\"]/span[3]/div[2]/div[3]/div/span/div/div/div[1]/div/div/a/div[1]/h2/span")).click();
-								
-				
+				driver.findElement(By.xpath("//*[@id=\"nav-hamburger-menu\"]")).click();
+				driver.findElement(By.xpath("//*[@id=\"hmenu-customer-name\"]")).click();
+				driver.findElement(By.xpath("//*[@id=\"register_accordion_header\"]/i")).click();
+				driver.findElement(By.xpath("//*[@id=\"ap_customer_name\"]")).sendKeys("Mohanraj");
+				driver.findElement(By.xpath("//*[@id=\"ap_phone_number\"]")).sendKeys("9880107799");
+				driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("rajmohan@1983");
+				driver.hideKeyboard();
+				Thread.sleep(5000);
+				driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+				String StrText =driver.findElement(MobileBy.xpath("//h4[text()='Mobile number already in use']")).getText();
+				System.out.println(StrText);
+		
 			
 				
 	}
--- src/browser/amazon_2.java
+++ src/browser/amazon_2.java
@@ -7,7 +7,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -15,29 +14,29 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class amazon_2 {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-				DesiredCapabilities cap = new DesiredCapabilities();
-				//cap.setCapability("deviceName","abc")
-				cap.setCapability(MobileCapabilityType.DEVICE_NAME, "abc");
-				cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
-				cap.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
-				cap.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "C:\\Training\\chromedriver_win32\\chromedriver.exe");
-				AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
-				driver.manage().timeouts().implicitlyWait(30,  TimeUnit.SECONDS);
-				driver.get("http://amazon.in");
-				driver.findElement(By.xpath("//*[@id=\"nav-hamburger-menu\"]")).click();
-				driver.findElement(By.xpath("//*[@id=\"hmenu-customer-name\"]")).click();
-				driver.findElement(By.xpath("//*[@id=\"register_accordion_header\"]/i")).click();
-				driver.findElement(By.xpath("//*[@id=\"ap_customer_name\"]")).sendKeys("Mohanraj");
-				driver.findElement(By.xpath("//*[@id=\"ap_phone_number\"]")).sendKeys("9880107799");
-				driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("rajmohan@1983");
-				driver.hideKeyboard();
-				Thread.sleep(5000);
-				driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
-				String StrText =driver.findElement(MobileBy.xpath("//h4[text()='Mobile number already in use']")).getText();
-				System.out.println(StrText);
-		
+			// TODO Auto-generated method stub
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "abc");
+			cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+			cap.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+			cap.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "C:\\Training\\chromedriver_win32\\chromedriver.exe");
+			AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.get("http://amazon.in");
+			driver.findElement(By.xpath("//*[@type='text']")).sendKeys("iphone 7s plus");
+			driver.findElement(By.xpath("//*[@class='suggest_link2']")).click();
+			
+			driver.findElement(By.xpath("//*[@class= 'a-size-base a-color-base a-text-normal']")).click();
+			driver.findElement(By.xpath("//*[@class= 'a-sheet-close a-icon a-icon-close-white a-focus-hidden']")).click();
+			
+			Thread.sleep(3000);
+			
+			driver.findElement(By.xpath("//*[@class='a-icon a-icon-supplemental']/following-sibling::input")).click();
+			driver.findElement(By.xpath("(//*[@class='a-button-input' and @type='submit'])[9]")).click();
+			driver.findElement(By.xpath("//*[@class='a-sheet-close a-focus-hidden']")).click();
+			driver.findElement(By.xpath("//*[@class='a-button-text a-declarative']")).click();
+			driver.findElement(By.xpath("//*[@id='mobileQuantityDropDown_4']")).click();
+			driver.findElement(By.xpath("//*[@class='a-button-text a-text-left']")).click();
 			
 				
 	}
